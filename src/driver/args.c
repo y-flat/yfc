a640f9e9c03b047fddcaf99ae37515e834a993e3
adamhutchings <adam.abahot@gmail.com>
2021-12-09 16:09:24 -0800

    Add some more skeleton

--- src/driver/args.c
+++ src/driver/args.c
@@ -7,3 +7,7 @@
 void yf_parse_args(int argc, char ** argv, struct yf_args * args) {
     /* TODO */
 }
+
+int yf_should_compile(struct yf_args * args) {
+    return 0; /* TODO */
+}
--- src/driver/args.h
+++ src/driver/args.h
@@ -15,4 +15,12 @@ struct yf_args {
  */
 void yf_parse_args(int argc, char ** argv, struct yf_args * args);
 
+/**
+ * Whether or not compilation should even happen. I know one might think it
+ * always should, but if the user does, say, yfc --help or --version, then no
+ * compilation should happen. This ALSO returns 0 if there's some error in the
+ * arguments given.
+ */
+int yf_should_compile(struct yf_args * args);
+
 #endif /* DRIVER_ARGS_H */

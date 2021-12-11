dad1c263acd4196d0c73a0afa42b8e208bd4e371
adamhutchings <adam.abahot@gmail.com>
2021-12-11 10:16:07 -0800

    Add native-compiler option documentation

--- src/driver/help.c
+++ src/driver/help.c
@@ -4,7 +4,8 @@ const char
     * VERSION_MSG = "Version: 0.0.1\n",
     * FLAGS_MSG = "Flags:\n"
       "-h, --help: Display this message.\n"
-      "-v, --version: Display version.\n",
+      "-v, --version: Display version.\n"
+      "-native-compiler <compiler>: specify the native C compiler to use.\n",
     * HELP_HINT_MSG = "Invalid command. "
       "Use \"-h\" or \"--help\" for a list of possible commands.\n",
     * NO_ARGS_MSG = "yfc: Use \"-h\" or \"--help\""

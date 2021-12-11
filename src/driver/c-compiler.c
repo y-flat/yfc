e4d39adcbfe9ade211e47203917c286f2d170aa3
adamhutchings <adam.abahot@gmail.com>
2021-12-11 09:48:37 -0800

    Add some framework for native compiler determiner

---
+++ src/driver/c-compiler.c
@@ -0,0 +1,38 @@
+#include "c-compiler.h"
+
+#include <stdio.h> /* sprintf */
+#include <stdlib.h> /* malloc */
+
+const char * YF_C_COMPILER[256];
+
+/**
+ * Internal - determine whether a compiler exists on this machine.
+ * This runs the command and sees if a non-existence error happens.
+ */
+int compiler_exists(const char * compiler) {
+    
+    /* The command to run with system */
+    char * command;
+    /* The return value */
+    int ret;
+    
+    /* Allocate buffer for command */
+    command = malloc(
+        strlen(compiler) +
+        23 + /* The chars in "which [] 2>/dev/null 1>&2" (not counting []) */
+        1 /* null terminator */
+    );
+
+    sprintf(command, "which %s 2>/dev/null 1>&2", compiler);
+
+    /* "which" returns an error if the command doesn't exist */
+    ret = system(command) != 0;
+
+    free(command);
+    return ret;
+
+}
+
+enum yf_c_compiler_status yf_determine_c_compiler(struct yf_args * args) {
+
+}
---
+++ src/driver/c-compiler.h
@@ -0,0 +1,34 @@
+/**
+ * Determine the compiler used to compile the generated C code.
+ */
+
+#ifndef DRIVER_C_COMPILER_H
+#define DRIVER_C_COMPILER_H
+
+#include <driver/args.h>
+
+/**
+ * An access to the compiler used to compile the generated C code.
+ */
+extern const char YF_C_COMPILER[256];
+
+enum yf_c_compiler_status {
+    YF_OK,
+    YF_SPECIFIED_COMPILER_NOT_FOUND,
+    YF_NO_COMPILER_FOUND,
+};
+
+/**
+ * Determine the compiler and fill the C_COMPILER variable with its value. USE
+ * THIS BEFORE USING C_COMPILER.
+ * It takes the args struct as input because the ussre can specify a C compiler
+ * to use.
+ * Returns:
+ * YF_OK - if a compiler was found and all is well.
+ * YF_SPECIFIED_COMPILER_NOT_FOUND - if the user specified a compiler but it
+ * does not exist on this machine
+ * YF_NO_COMPILER_FOUND - if no compiler was found on this machine
+ */
+enum yf_c_compiler_status yf_determine_c_compiler(struct yf_args *);
+
+#endif /* DRIVER_C_COMPILER_H */

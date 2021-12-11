17b31378c71d4cd0ac296b9b7a0fa21c4ff551dc
adamhutchings <adam.abahot@gmail.com>
2021-12-11 10:13:54 -0800

    Finish compiler-finding algorithm

--- src/driver/c-compiler.c
+++ src/driver/c-compiler.c
@@ -2,9 +2,9 @@
 
 #include <stdio.h> /* sprintf */
 #include <stdlib.h> /* malloc */
-#include <string.h> /* strlen */
+#include <string.h> /* strlen, strcpy */
 
-const char YF_C_COMPILER[256];
+char YF_C_COMPILER[256];
 
 /**
  * Internal - determine whether a compiler exists on this machine.
@@ -35,5 +35,45 @@ int compiler_exists(const char * compiler) {
 }
 
 enum yf_c_compiler_status yf_determine_c_compiler(struct yf_args * args) {
-    return YF_OK;
+    
+    /* Check if the user has specified a compiler */
+    if (args->compiler) {
+        if (compiler_exists(args->compiler)) {
+            /* Needs to be a short enough name! */
+            if (strlen(args->compiler) > 255) {
+                return YF_COMPILER_NAME_OVERFLOW;
+            } else {
+                strcpy(YF_C_COMPILER, args->compiler);
+                return YF_COMPILER_OK;
+            }
+        } else {
+            return YF_SPECIFIED_COMPILER_NOT_FOUND;
+        }
+    }
+
+/* Set the compiler. I don't want to type this out for a million names. */
+#define SET_COMPILER_IF_POSSIBLE(name) do { \
+    if (compiler_exists(name)) { \
+        strcpy(YF_C_COMPILER, name); \
+        return YF_COMPILER_OK; \
+    } \
+} while (0)
+
+    /* clang is better. I said it. */
+    SET_COMPILER_IF_POSSIBLE("clang");
+
+    /* NOW try gcc. */
+    SET_COMPILER_IF_POSSIBLE("gcc");
+
+    /* Now some others. */
+    SET_COMPILER_IF_POSSIBLE("cc");
+    SET_COMPILER_IF_POSSIBLE("tcc");
+
+    /* TODO - add more? */
+
+    /* None found. */
+    return YF_NO_COMPILER_FOUND;
+
+#undef SET_COMPILER_IF_POSSIBLE
+
 }

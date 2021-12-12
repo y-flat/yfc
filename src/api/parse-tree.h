bcbbda645ab316b4ac5613c34c43ef3a643bcbdd
adamhutchings <adam.abahot@gmail.com>
2021-12-11 18:13:04 -0800

    Add parse tree outline

---
+++ src/api/parse-tree.h
@@ -0,0 +1,16 @@
+/**
+ * All parse tree data structure definitions.
+ */
+
+#ifndef API_AST_H
+#define API_AST_H
+
+/**
+ * This is the output of the lexer and parser. No semantic definitions, only
+ * syntax checks.
+ */
+struct yf_parse_tree {
+    /* TODO */
+};
+
+#endif /* API_AST_H */

b3301fb13d18adfbf2cc484184bbd892eaffe75e
adamhutchings <adam.abahot@gmail.com>
2021-12-11 18:20:32 -0800

    Update header to be accurate

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -6,9 +6,12 @@
 #ifndef PARSER_PARSER_H
 #define PARSER_PARSER_H
 
+#include <api/parse-tree.h>
+
 /**
+ * Parse data from file into tree.
  * Returns: error code, or 0 if successful.
  */
-int yf_parse_file(const char *);
+int yf_parse_file(const char * filename, struct yf_parse_tree * tree);
 
 #endif /* PARSER_PARSER_H */

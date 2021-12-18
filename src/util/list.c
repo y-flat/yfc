10ff623f30af275611602c6e876ac19f9ef0714c
adamhutchings <adam.abahot@gmail.com>
2021-12-17 23:15:36 -0800

    Add list implementation

---
+++ src/util/list.c
@@ -0,0 +1,97 @@
+#include "list.h"
+
+#include <stdlib.h>
+
+#include <util/allocator.h>
+
+int yf_list_init(struct yf_list * list) {
+    list->current = NULL;
+    list->current_index = YF_LIST_BLOCK_SIZE;
+    return 0;
+}
+
+int yf_list_get(struct yf_list * list, void ** elem) {
+
+    if (!list->current || (list->current_index > list->current->numfull)) {
+        return -1;
+    }
+
+    *elem = list->current->data[list->current_index];
+    return 0;
+
+}
+
+int yf_list_next(struct yf_list * list) {
+
+    if (list->current_index == YF_LIST_BLOCK_SIZE - 1) {
+        if (list->current->next == NULL) {
+            return -1;
+        } else {
+            list->current = list->current->next;
+            list->current_index = 0;
+        }
+    } else {
+        ++list->current_index;
+        if (list->current_index > list->current->numfull) {
+            return -1;
+        }
+    }
+
+    return 0;
+
+}
+
+void yf_list_reset(struct yf_list * list) {
+
+    list->current = list->first;
+    list->current_index = 0;
+
+}
+
+void yf_list_add(struct yf_list * list, void * element) {
+
+    struct yf_list_block * block = list->current;
+
+    /* Get to the end of the list. */
+    for (;;) {
+        if (block == NULL || block->next == NULL) {
+            break;
+        }
+        block = block->next;
+    }
+
+    if (!block || (block->numfull = YF_LIST_BLOCK_SIZE)) {
+        if (!block) {
+            block = yf_malloc(sizeof (struct yf_list_block));
+        } else {
+            block->next = yf_malloc(sizeof (struct yf_list_block));
+            block = block->next;
+        }
+        if (!block) {
+            return -1;
+        }
+        memset(block, 0, sizeof (struct yf_list_block));
+        list->current_index = 0;
+    }
+
+    block->data[list->current_index] = element;
+    ++block->numfull;
+
+}
+
+void yf_list_destroy(struct yf_list * list) {
+
+    struct yf_list_block * block = list->current;
+    struct yf_list_block * last; /* See below */
+
+    if (!block) return;
+
+    /* Go and free all blocks. */
+    for (;;) {
+        last = block;
+        block = block->next;
+        yf_free(last);
+        if (!block) break;
+    }
+
+}
---
+++ src/util/list.h
@@ -0,0 +1,71 @@
+/**
+ * Linked list implementation.
+ */
+
+#ifndef UTIL_LIST_H
+#define UTIL_LIST_H
+
+/**
+ * How it works - each linked list is theoretically a list of nodes with a
+ * pointer to the next one. But that's kind of slow in practice, and a waste of
+ * pointers, so what we're going to do is to have a linked list of blocks
+ * of memory, with some number of elements in each block.
+ */
+
+#define YF_LIST_BLOCK_SIZE 64
+
+struct yf_list_block {
+    struct yf_list_block * next;
+    void * data[YF_LIST_BLOCK_SIZE];
+    int numfull; /* The first empty block. */
+};
+
+/**
+ * It's only a pointer to the first block and an indication of where we are.
+ */
+struct yf_list {
+
+    struct yf_list_block * first;
+
+    /* Current view */
+    struct yf_list_block * current;
+    int current_index;
+
+};
+
+/**
+ * The public API functions for lists - below
+ */
+
+/**
+ * Initialize a new list. Return -1 if memory allocation has failed.
+ */
+int yf_list_init(struct yf_list * list);
+
+/**
+ * Get the currently pointed-to element. Returns -1 if we've passed the end, or
+ * 0 otherwise.
+ */
+int yf_list_get(struct yf_list * list, void ** elem);
+
+/**
+ * Move to the next element. Return -1 if we've passed the end, or 0.
+ */
+int yf_list_next(struct yf_list * list);
+
+/**
+ * Reset to the beginning.
+ */
+void yf_list_reset(struct yf_list * list);
+
+/**
+ * Add an element. Returns -1 if we've run out of memory, or 0 otherwise.
+ */
+int yf_list_add(struct yf_list * list, void * element);
+
+/**
+ * Destroy a list.
+ */
+void yf_list_destroy(struct yf_list * list);
+
+#endif /* UTIL_LIST_H */

4af8ec87bf5e6d29f9319057355595fb53db8c3a
adamhutchings <adam.abahot@gmail.com>
2021-12-20 23:26:48 -0800

    Fix up some decl locations

--- src/parser/expr.c
+++ src/parser/expr.c
@@ -3,4 +3,34 @@
  * own file. Yikes!
  */
 
+#include <string.h>
 
+#include <parser/parser-internals.h>
+
+int yfp_expr(struct yf_parse_node * node, struct yf_lexer * lexer) {
+    
+    struct yf_token tok;
+
+    node->type = YFCS_EXPR;
+    
+    yfl_lex(lexer, &tok);
+    /* TODO - handle recursive exprs */
+    switch (tok.type) {
+    case YFT_IDENTIFIER:
+        yfl_unlex(lexer, &tok);
+        yfp_ident(&node->as.expr.value.as.identifier, lexer);
+        break;
+    case YFT_LITERAL:
+        strcpy(
+            node->as.expr.value.as.literal.value.databuf, tok.data
+        );
+        node->as.expr.value.type = YFCS_LITERAL;
+        break;
+    default:
+        YF_TOKERR(tok, "identifier or literal");
+        break;
+    }
+
+    return 0;
+
+}
--- src/parser/parser-internals.h
+++ src/parser/parser-internals.h
@@ -1,12 +1,37 @@
 #ifndef PARSER_PARSER_INTERNALS_H
 #define PARSER_PARSER_INTERNALS_H
 
-static int yfp_program(struct yf_parse_node * node, struct yf_lexer * lexer);
-static int yfp_vardecl(struct yf_parse_node * node, struct yf_lexer * lexer);
-static int yfp_funcdecl(struct yf_parse_node * node, struct yf_lexer * lexer);
-static int yfp_stmt(struct yf_parse_node * node, struct yf_lexer * lexer);
-static int yfp_expr(struct yf_parse_node * node, struct yf_lexer * lexer);
-static int yfp_ident(struct yfcs_identifier * node, struct yf_lexer * lexer);
-static int yfp_type(struct yfcs_type * node, struct yf_lexer * lexer);
+/* All includes for every parser file */
+
+#include <api/concrete-tree.h>
+#include <api/tokens.h>
+#include <lexer/lexer.h>
+#include <util/allocator.h>
+#include <util/yfc-out.h>
+
+/**
+ * Print error if token type unexpected.
+ * Example: YF_TOKERR(tok, "comma or colon")
+ */
+#define YF_TOKERR(tok, expected) do { \
+    YF_PRINT_ERROR( \
+        "Unexpected token %s, line %d column %d:" \
+        "expected %s, found token of type \"%s\"", \
+        tok.data, \
+        tok.lineno, \
+        tok.colno, \
+        expected, \
+        yf_get_toktype(tok.type) \
+    ); \
+    return 4; /* TODO */ \
+} while (0)
+
+int yfp_program(struct yf_parse_node * node, struct yf_lexer * lexer);
+int yfp_vardecl(struct yf_parse_node * node, struct yf_lexer * lexer);
+int yfp_funcdecl(struct yf_parse_node * node, struct yf_lexer * lexer);
+int yfp_stmt(struct yf_parse_node * node, struct yf_lexer * lexer);
+int yfp_expr(struct yf_parse_node * node, struct yf_lexer * lexer);
+int yfp_ident(struct yfcs_identifier * node, struct yf_lexer * lexer);
+int yfp_type(struct yfcs_type * node, struct yf_lexer * lexer);
 
 #endif /* PARSER_PARSER_INTERNALS_H */

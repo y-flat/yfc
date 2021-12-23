c644b9c76ce9fc405fcc9fc6e92fa62c6320027e
adamhutchings <adam.abahot@gmail.com>
2021-12-22 16:50:52 -0800

    Hopefully complete binary expr parsing

--- src/parser/expr.c
+++ src/parser/expr.c
@@ -5,6 +5,7 @@
 
 #include <string.h>
 
+#include <api/operator.h>
 #include <parser/parser-internals.h>
 
 /**
@@ -44,9 +45,132 @@ int yfp_atomic_expr(struct yf_parse_node * node, struct yf_lexer * lexer) {
 
 }
 
+static int yfp_sort_expr_tree(
+    struct yf_parse_node * nodes, int num_nodes,
+    enum yf_operator * operators, /* num_operators = num_nodes - 1 */
+    struct yfcs_expr * node
+);
+
+/**
+ * Return values:
+ * 0 - all OK
+ * 1 - too many subbranches
+ * 2 - invalid operator
+ */
 int yfp_expr(struct yf_parse_node * node, struct yf_lexer * lexer) {
 
-    /* TODO - parse multiple branches */
-    return yfp_atomic_expr(node, lexer);
+    struct yf_parse_node atomics[64];
+    enum yf_operator operators[63];
+
+    struct yf_token tok;
+
+    int i, lex_err;
+
+    /**
+     * Explanation for this stage of the parsing algorithm - get all atomic
+     * expressions and operators. Like so:
+     * (a + b) * c(d) - e
+     * atomics, (a + b), c(d), e
+     * operators, *, -
+     */
+
+    /**
+     * We parse one atomic expr first, and then we parse [op], [atomic expr]
+     * until done.
+     */
+    if (yfp_atomic_expr(&atomics[0], lexer)) {
+        return 4;
+    }
+
+    for (i = 0; i < 64; i++) {
+        P_LEX(lexer, &tok);
+        if (tok.type == YFT_OP) {
+            operators[i] = yf_get_operator(tok.data);
+            if (operators[i] == YFO_INVALID) {
+                /* TODO - error message */
+                YF_TOKERR(tok, "valid operator");
+                return 2;
+            }
+            if (yfp_atomic_expr(&atomics[i + 1], lexer)) {
+                return 4;
+            }
+        } else {
+            /* Not an error - we've simply reached the end. */
+            yfl_unlex(lexer, &tok);
+            break;
+        }
+    }
+
+    /* Now we have all operators and atomics. */
+    return yfp_sort_expr_tree(atomics, i + 1, operators, &node->as.expr);
+
+}
+
+/**
+ * Sorts the child <nodes> into a tree, where <node> is the pointer to that
+ * tree.
+ */
+static int yfp_sort_expr_tree(
+    struct yf_parse_node * nodes, int num_nodes,
+    enum yf_operator * operators,
+    struct yfcs_expr * node
+) {
+
+    int i, index;
+    enum yfo_precedence prec;
+
+    /* First, trivial cases. */
+    if (num_nodes == 1) {
+        *node = nodes[0].as.expr;
+        return 0;
+    }
+    if (num_nodes == 2) {
+        node->type = YFCS_BINARY;
+        node->as.binary.op = operators[0];
+        node->as.binary.left = malloc(sizeof(struct yf_parse_node));
+        node->as.binary.right = malloc(sizeof(struct yf_parse_node));
+        if (!node->as.binary.left || !node->as.binary.right) {
+            return 1;
+        }
+        *node->as.binary.left = nodes[0].as.expr;
+        *node->as.binary.right = nodes[1].as.expr;
+        return 0;
+    }
+
+    /**
+     * We find the location of the lowest-precedence operator, and then split
+     * the expression tree based on it. If there are multiple tied operators,
+     * we choose the first one if right-associative, and the last one if left.
+     */
+    for (i = 0, index = -1; i < num_nodes - 1; i++) {
+        prec = yfo_prec(operators[i], operators[index]);
+        if (prec == LESS || index == -1) {
+            index = i;
+        }
+        /* If left-associative and tied, we want the last. */
+        if (prec == EQUAL && 
+            yf_get_operator_assoc(operators[i]) == YFOA_LEFT
+        ) {
+            index = i;
+        }
+    }
+
+    /* Now that we have our splitting location, we recurse. */
+    node->type = YFCS_BINARY;
+    node->as.binary.left = malloc(sizeof(struct yf_parse_node));
+    node->as.binary.right = malloc(sizeof(struct yf_parse_node));
+    yfp_sort_expr_tree(
+        nodes,
+        index,
+        operators,
+        node->as.binary.left
+    );
+    yfp_sort_expr_tree(
+        nodes + index + 1,
+        num_nodes - index,
+        operators + index + 1,
+        node->as.binary.right
+    );
+    return 0;
 
 }

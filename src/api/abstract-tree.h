c07b98a396fc248b97b24c1aac5aabf1d526b848
adamhutchings <adam.abahot@gmail.com>
2021-12-27 23:29:32 -0800

    Fully flesh out abstract tree

--- src/api/abstract-tree.h
+++ src/api/abstract-tree.h
@@ -6,29 +6,78 @@
  * As C is a fully explicit language, there will be nothing explicit about this
  * tree - all default parameters are filled in, all constructors are called
  * explicitly, and so forth.
+ * 
+ * This tree structure will look very similar to the CST ... because it is.
  */
 
 #ifndef API_ABSTRACT_TREE_H
 #define API_ABSTRACT_TREE_H
 
+#include <api/operator.h>
+#include <api/sym.h>
+#include <util/list.h>
+
+struct yf_ast_node;
+
+struct yfa_value {
+
+    enum {
+        YFCS_IDENT,
+        YFCS_LITERAL,
+    } type;
+
+    union {
+        struct yf_sym * identifier;
+        struct {
+            enum {
+                YFCS_NUM, /* TODO */
+            } type;
+            union {
+                int val;
+            };
+        } literal;
+    } as;
+
+};
+
 struct yfa_expr {
-    /* TODO */
+    union {
+        struct yfa_value value;
+        struct {
+            struct yfa_expr *left;
+            struct yfa_expr *right;
+            enum yf_operator op;
+        } binary;
+    } as;
+
+    enum {
+        YFA_VALUE,
+        YFA_BINARY,
+    } type;
+    
 };
 
 struct yfa_vardecl {
-    /* TODO */
+    struct yf_sym * name;
+    struct yfs_type type;
+    /* Can be NULL, and is always of type expr, is the right-hand side */
+    struct yf_ast_node * expr;
 };
 
 struct yfa_funcdecl {
-    /* TODO */
+    struct yf_sym * name;
+    struct yfs_type ret; /* The return type */
+    /* All parameters are stored as vardecls. expr WILL be null for these. */
+    struct yf_list params;
+    struct yf_ast_node * body; /* The function body */
 };
 
 struct yfa_program {
-    /* TODO */
+    struct yf_list decls;
 };
 
 struct yfa_bstmt {
-    /* TODO */
+    struct yf_list stmts;
 };
 
 struct yf_ast_node {

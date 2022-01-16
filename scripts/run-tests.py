dada9d13a770f4edf899026f29f6c667416fb6a5
adamhutchings <adam.abahot@gmail.com>
2022-01-15 22:22:39 -0800

    Add int literal parsing

--- scripts/run-tests.py
+++ scripts/run-tests.py
@@ -58,6 +58,7 @@ def main():
     add_semantic_test("tests/sem/duplicate-decl-local.yf",   False)
     add_semantic_test("tests/sem/funcs-fail.yf",             False)
     add_semantic_test("tests/sem/funcs-pass.yf",             True )
+    add_semantic_test("tests/sem/invalid-int-literal.yf",    False)
     add_semantic_test("tests/sem/no-value-assign.yf",        False)
     add_semantic_test("tests/sem/shadow.yf",                 True )
     add_semantic_test("tests/sem/type-val-pass.yf",          True )

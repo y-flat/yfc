ee273180b1e40750c4cac83b921cd5fe1cbd5a59
adamhutchings <adam.abahot@gmail.com>
2022-01-25 20:14:44 -0800

    Start index at 0

--- src/parser/expr.c
+++ src/parser/expr.c
@@ -196,7 +196,7 @@ static int yfp_sort_expr_tree(
      * the expression tree based on it. If there are multiple tied operators,
      * we choose the first one if right-associative, and the last one if left.
      */
-    for (i = 0, index = -1; i < num_nodes - 1; i++) {
+    for (i = 0, index = 0; i < num_nodes - 1; i++) {
         prec = yfo_prec(operators[i], operators[index]);
         if (prec == LESS || index == -1) {
             index = i;

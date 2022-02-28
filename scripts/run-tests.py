83c09fa11c34871264e0659845c72b412c26207b
dekrain <dawidkrainski8@gmail.com>
2022-02-28 18:48:09 +0100

    Improve testing framework

--- scripts/run-tests.py
+++ scripts/run-tests.py
@@ -1,80 +1,69 @@
-"""Run tests. Each test file is paired with a value of
-whether it should pass or not."""
+#!/bin/env python3
 
-import os
+"""
+Run tests.
+Each test file is paired with a value of whether it should pass or not.
+"""
+
+import json
+import os, os.path
+import subprocess
 import sys
 
 tests = []
 
 def add_test(test, flags, pass_):
-    tests.append((f"{test} {flags}", pass_))
+    tests.append( (test, flags, pass_) )
 
-def add_lexer_test(test, pass_):
-    add_test(f"{test}", "--dump-tokens", pass_)
-
-def add_parser_test(test, pass_):
-    add_test(f"{test}", "--dump-cst", pass_)
-
-def add_semantic_test(test, pass_):
-    add_test(f"{test}", "--just-semantics", pass_)
-
+yfc_path = './cmake/yfc'
 def run_tests():
+
     total = passed = failed = 0
     failed_files = []
-    # Cursed. TODO: get system success value in a better way
-    for test, pass_ in tests:
-        ret = os.system(f"./cmake/yfc {test} &>/dev/null")
-        ret = (ret == 0) # ret = True is a pass of the file, False is a fail
-        if (ret != pass_):
-            print(f"\033[91mFAIL: {test.split()[0]}\033[0m")
+
+    for test, flags, pass_ in tests:
+        retcode = subprocess.call( (yfc_path, *flags, test),
+            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL)
+        success = retcode == 0
+        if (success != pass_):
+            print(f"\033[91mFAIL: {test}\033[0m")
             failed += 1
             failed_files.append(test)
         else:
-            print(f"\033[92mPass: {test.split()[0]}\033[0m")
+            print(f"\033[92mPass: {test}\033[0m")
             passed += 1
         total += 1
+
     print(f"{passed}/{total} passed")
     if failed > 0:
         print(f"Failed tests:")
         for file in failed_files:
-            print(f"\t{file.split()[0]}")
+            print(f"\t{file}")
         return 1
     return 0
 
 # TODO - more detailed result checking than just "pass/fail"
+test_dir = "tests"
 def main():
 
-    add_lexer_test("tests/lexer/broken-comment.yf",          False)
-    add_lexer_test("tests/lexer/keyword-test.yf",            True )
-    add_lexer_test("tests/lexer/large-token.yf",             False)
-    add_lexer_test("tests/lexer/lexer-test.yf",              True )
+    if not os.path.exists(yfc_path):
+        print(f"\033[91mFATAL ERROR: Compiler executable {yfc_path!r} not found. Maybe you forgot to build it?\033[0m")
+        return 2
 
-    add_parser_test("tests/parser/funccall-parsing.yf",      True )
-    add_parser_test("tests/parser/funcdecl-parsing.yf",      True )
-    add_parser_test("tests/parser/op-parsing.yf",            True )
-    add_parser_test("tests/parser/return-stmt.yf",           True )
-    add_parser_test("tests/parser/vardecl-neq.yf",           False)
-    add_parser_test("tests/parser/vardecl-test.yf",          True )
-
-    add_semantic_test("tests/sem/duplicate-decl-global.yf",  False)
-    add_semantic_test("tests/sem/duplicate-decl-local.yf",   False)
-    add_semantic_test("tests/sem/empty-stmt.yf",             True )
-    add_semantic_test("tests/sem/funccall-bad.yf",           False)
-    add_semantic_test("tests/sem/funccall-good.yf",          True )
-    add_semantic_test("tests/sem/funccall-types.yf",         False)
-    add_semantic_test("tests/sem/funcs-fail.yf",             False)
-    add_semantic_test("tests/sem/funcs-pass.yf",             True )
-    add_semantic_test("tests/sem/if-condition-not-bool.yf",  False)
-    add_semantic_test("tests/sem/if-good.yf",                True )
-    add_semantic_test("tests/sem/invalid-int-literal.yf",    False)
-    add_semantic_test("tests/sem/no-value-assign.yf",        False)
-    add_semantic_test("tests/sem/return-bad.yf",             False)
-    add_semantic_test("tests/sem/return-block.yf",           True )
-    add_semantic_test("tests/sem/return-good.yf",            True )
-    add_semantic_test("tests/sem/return-warn.yf",            True )
-    add_semantic_test("tests/sem/shadow.yf",                 True )
-    add_semantic_test("tests/sem/type-val-pass.yf",          True )
-    add_semantic_test("tests/sem/unknown-type.yf",           False)
+    for dir in os.scandir(test_dir):
+        if not dir.is_dir():
+            continue
+        if not os.path.exists(os.path.join(dir.path, "index.json")):
+            continue
+        with open(os.path.join(dir.path, "index.json")) as f:
+            index = json.load(f)
+        flags = index['flags'] if 'flags' in index else []
+        for unit, sig in index['tests'].items():
+            testfile = os.path.join(dir.path, unit + '.yf')
+            if not os.path.exists(testfile):
+                print(f"\033[93mWarning: Test {dir.name}/{unit} does not exist\033[0m")
+                continue
+            add_test(testfile, flags, sig['pass'])
 
     return run_tests()
 

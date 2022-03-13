cce0921a60f6f615d7081eccda03692004a569de
adamhutchings <adam.abahot@gmail.com>
2022-03-13 13:03:16 -0700

    Add genreation struct

---
+++ src/api/generation.h
@@ -0,0 +1,15 @@
+/**
+ * Structures for code generation.
+ */
+
+#ifndef API_GENERATION_H
+#define API_GENERATION_H
+
+struct yf_gen_info {
+
+    char * prefix; /* For the prefix of generated identifiers. */
+    int tab_depth; /* For the indentation level and proper formatting. */
+
+};
+
+#endif /* API_GENERATION_H */

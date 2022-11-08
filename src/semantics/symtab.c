1824f35d8beca55f9705a1051c0f98e5230b0ecb
adamhutchings <adam.abahot@gmail.com>
2022-11-08 10:46:43 -0500

    Add proper type lookup for function symbols

--- src/semantics/symtab.c
+++ src/semantics/symtab.c
@@ -7,8 +7,10 @@
 #include <util/allocator.h>
 #include <util/yfc-out.h>
 
-static int yfs_add_var(struct yf_hashmap * symtab, struct yf_parse_node *);
-static int yfs_add_fn(struct yf_hashmap * symtab, struct yf_parse_node *);
+static int yfs_add_var(struct yf_compile_analyse_job *,
+struct yf_hashmap * symtab, struct yf_parse_node *);
+static int yfs_add_fn(struct yf_compile_analyse_job *,
+struct yf_hashmap * symtab, struct yf_parse_node *);
 
 int yfs_build_symtab(struct yf_compile_analyse_job * data) {
 
@@ -29,11 +31,11 @@ int yfs_build_symtab(struct yf_compile_analyse_job * data) {
     YF_LIST_FOREACH(data->parse_tree.program.decls, node) {
         switch (node->type) {
             case YFCS_VARDECL:
-                if (yfs_add_var(&data->symtab.table, node))
+                if (yfs_add_var(data, &data->symtab.table, node))
                     ret = 1;
                 break;
             case YFCS_FUNCDECL:
-                if (yfs_add_fn(&data->symtab.table, node))
+                if (yfs_add_fn(data, &data->symtab.table, node))
                     ret = 1;
                 break;
             default:
@@ -46,7 +48,10 @@ int yfs_build_symtab(struct yf_compile_analyse_job * data) {
 
 }
 
-static int yfs_add_var(struct yf_hashmap * symtab, struct yf_parse_node * n) {
+static int yfs_add_var(
+    struct yf_compile_analyse_job * data,
+    struct yf_hashmap * symtab, struct yf_parse_node * n
+) {
 
     struct yfcs_vardecl * v = &n->vardecl;
     struct yf_sym * vsym, * dupl;
@@ -74,7 +79,10 @@ static int yfs_add_var(struct yf_hashmap * symtab, struct yf_parse_node * n) {
 
 }
 
-static int yfs_add_fn(struct yf_hashmap * symtab, struct yf_parse_node * f) {
+static int yfs_add_fn(
+    struct yf_compile_analyse_job * udata, struct yf_hashmap * symtab,
+    struct yf_parse_node * f
+) {
     
     struct yfcs_funcdecl * fn = &f->funcdecl;
 
@@ -109,8 +117,7 @@ static int yfs_add_fn(struct yf_hashmap * symtab, struct yf_parse_node * f) {
     }
 
     /* TODO -- actually look up custom types once those exist. */
-    fsym->fn.rtype.kind = YFS_T_PRIMITIVE;
-    fsym->fn.rtype.name = fn->ret.databuf;
+    fsym->fn.rtype = *yfv_get_type_s(udata, fn->ret.databuf);
 
     yfh_set(symtab, fsym->fn.name, fsym);
 
